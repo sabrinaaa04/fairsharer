11156d81fdd6eb73c91a830f64349c1ebf3923c4
sabrinaaa04 <dnbc84ck2h@privaterelay.appleid.com>
2026-01-07 15:41:23 +0100

    Update test_fairsharer.py

--- test/test_fairsharer.py
+++ test/test_fairsharer.py
@@ -1,1 +1,12 @@
+import numpy as np
+from fairsharer.fair_sharer import fair_sharer
 
+
+def test_fair_sharer_examples():
+    values = [0, 1000, 800, 0]
+
+    result_1 = fair_sharer(values, 1)
+    assert np.allclose(result_1, [100, 800, 900, 0])
+
+    result_2 = fair_sharer(values, 2)
+    assert np.allclose(result_2, [100, 890, 720, 90])
